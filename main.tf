6897ba2759b9a574666adbe4edcd673cf72889da
nihalm97 <nihalmulakala@gmail.com>
2024-03-28 18:13:11 -0500

    commiting a change

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "buckettest77"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
